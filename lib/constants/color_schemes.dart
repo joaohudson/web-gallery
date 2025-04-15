bd71208a8c8ebc83bf9da8741ecbf597985d7bfe
João Hudson <joao.hudson@taking.com.br>
2025-04-14 22:39:19 -0300

    fix: error color

--- lib/constants/color_schemes.dart
+++ lib/constants/color_schemes.dart
@@ -6,7 +6,7 @@ const defaultDark = ColorScheme(
   onPrimary: Color.fromARGB(255, 94, 94, 94),
   secondary: Color.fromARGB(255, 19, 19, 19),
   onSecondary: Color.fromARGB(255, 30, 202, 233),
-  error: Color(0xFFF48771),
+  error: Color.fromARGB(255, 197, 53, 24),
   onError: Colors.black,
   background: Color(0xFF1E1E1E),
   onBackground: Color.fromARGB(255, 187, 187, 187),
