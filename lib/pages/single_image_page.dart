935ed4294c0d73b1d4eb543ee407a2078ffa55ff
João Hudson <joao.hudson@taking.com.br>
2025-05-20 20:59:10 -0300

    feat: add pagination in single page mode

--- lib/pages/single_image_page.dart
+++ lib/pages/single_image_page.dart
@@ -2,29 +2,87 @@ import 'package:flutter/material.dart';
 import 'package:webgallery/components/web_image.dart';
 import 'package:webgallery/core/gallery_manager.dart';
 
-class SingleImagePage extends StatelessWidget {
+class SingleImagePage extends StatefulWidget {
   final int index;
   final Gallery gallery;
-  const SingleImagePage({required this.gallery, required this.index, super.key});
+  const SingleImagePage(
+      {required this.gallery, required this.index, super.key});
+  @override
+  State<SingleImagePage> createState() => _SingleImagePageState();
+}
+
+class _SingleImagePageState extends State<SingleImagePage> {
+  int currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    currentIndex = widget.index;
+  }
+
+  bool inFirstImage() {
+    return currentIndex == 0;
+  }
+
+  bool inLastImage() {
+    return currentIndex == widget.gallery.urls.length - 1;
+  }
+
+  void nextPage() {
+    setState(() {
+      if (!inLastImage()) {
+        currentIndex++;
+      }
+    });
+  }
+
+  void previousPage() {
+    setState(() {
+      if (!inFirstImage()) {
+        currentIndex--;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.sizeOf(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('${gallery.name} - $index'),
+        title: Text(
+            '${widget.gallery.name} - ${currentIndex + 1} / ${widget.gallery.urls.length}'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       body: InteractiveViewer(
-          minScale: 0.5,
-          maxScale: 8.0,
-          child: SizedBox(
-              width: MediaQuery.sizeOf(context).width,
-              height: MediaQuery.sizeOf(context).height,
-              child: Center(
-                child: Container(
-                  decoration: const BoxDecoration(color: Colors.black),
-                  child: WebImage(gallery.urls[index]),
-                ),
-              ))),
+        minScale: 0.5,
+        maxScale: 8.0,
+        child: SizedBox(
+          width: screenSize.width,
+          height: screenSize.height,
+          child: Center(
+            child: Container(
+              decoration: const BoxDecoration(color: Colors.black),
+              child: WebImage(widget.gallery.urls[currentIndex]),
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Theme.of(context).colorScheme.inversePrimary,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: inFirstImage() ? null : previousPage,
+              icon: const Icon(Icons.arrow_back),
+            ),
+            IconButton(
+              onPressed: inLastImage() ? null : nextPage,
+              icon: const Icon(Icons.arrow_forward),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
